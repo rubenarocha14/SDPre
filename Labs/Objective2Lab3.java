2994566e4dd7c8d3630cc9894022a4b96ce5faae
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-05 22:41:45 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    //variables for birth year, month and whether today is a weekend
+    int birthYear = 1995;
+    String birthMonth = "November";
+    boolean weekend = false;
+    //print out the variables
+    System.out.println(birthYear);
+    System.out.println(birthMonth);
+    System.out.println(weekend);
+  }
+}
