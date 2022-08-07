a4b9fb166d359ffab23f67210528edc60df7c657
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-07 07:58:03 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2{
+  public static void main(String[] args){
+    double side1, side2, hypotenuse;
+    side1 = 3;
+    side2 = 4;
+    hypotenuse = Math.sqrt((side1*side1)+(side2*side2));
+    System.out.println(hypotenuse);
+  }
+}
