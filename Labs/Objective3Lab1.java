d63f17004991371d791022160d8a06c5b5e1368f
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-07 07:25:22 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    // assign values to the numX variables
+    num1 = 37.2;
+    num2 = 25.6;
+    num3 = 15.4;
+    //calculate the average of the 3 values
+    average = (num1+num2+num3)/3;
+    System.out.println(average);
+  }
+}
