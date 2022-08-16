82f637a23b3c3d66cb9e64dd176b40e57443e689
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-16 07:54:47 -0600

    complete objective9lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,21 @@
+public class Objective9Lab2 {
+  public static void main(String[] args) {
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+  }
+
+  public static void printMessage() {
+    System.out.println("In method");
+    /*
+    Create a file called Objective9Lab2.java in the SDPre/Labs directory.
+    Using the Starter Code below add code to the commented areas that would:
+    Print out "In method".
+
+    Expected output
+    In main
+    In method
+    In main
+    */
+  }
+}
