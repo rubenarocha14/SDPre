2a15cf13b759674438ddef28bc11d308449b0032
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-15 21:54:53 -0600

    complete objective8Lab1

--- Labs/Objective8Lab1.java
+++ Labs/Objective8Lab1.java
@@ -4,6 +4,9 @@ public class Objective8Lab1 {
     for (i=1; i<11; i++){
       System.out.println(i);
     }
+    for (i=10; i>0; i--){
+      System.out.println(i);
+    }
     //TODO Write a for loop that will print the numbers 1-10
 
     //TODO Write another for loop that will print the numbers 10-1.
