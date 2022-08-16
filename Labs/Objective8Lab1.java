6ba3c06bf86dd7f16b46bcf611c2315e767a8077
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-15 21:49:51 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int i;
+    for (i=1; i<11; i++){
+      System.out.println(i);
+    }
+    //TODO Write a for loop that will print the numbers 1-10
+
+    //TODO Write another for loop that will print the numbers 10-1.
+
+
+  }
+}
