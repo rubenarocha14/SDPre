209e7c771ed8df9386b63e3c07d7d4c748091d37
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-15 22:58:20 -0600

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3{
+  public static void main(String[] args){
+    int i = 1;
+
+    for(i=1;i<=20;i++){
+      System.out.print(i);
+      if(i%2==0){
+        System.out.print(" is even.\n");
+      }
+      else{
+        System.out.print(" is odd.\n");
+      }
+    }
+  }
+}
