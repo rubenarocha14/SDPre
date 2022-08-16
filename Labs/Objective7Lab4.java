bd56a0611d70c64b85310d1cf4cd8110c242b278
rubenarocha14 <rubenarocha14@gmail.com>
2022-08-15 21:24:33 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int counter = 1;
+    int limit = 20;
+    int sum =0;
+
+    while(counter <= limit){
+      sum = sum + counter;
+      counter++;
+
+      }
+      System.out.println(sum);
+    }
+  }
